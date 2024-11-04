94a08393eaa89804e3c467acfa0fd82d1eec7da4
Daniel Araújo Berriel <127857248+DanielBerriel@users.noreply.github.com>
2024-11-04 18:34:29 +0000

    adiciona `.prettierignore`

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,4 @@ function Home() {
   return <h1>Isabela *_*</h1>;
 }
 
-function teste() {
-  console.log("Teste");
-}
-
-function teste2() {
-  console.log("indentação errada");
-}
-
 export default Home;
