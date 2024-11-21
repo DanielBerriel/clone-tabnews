16d75e0b354bd5d0ef055a27c6d7eac18e524439
Daniel Araújo Berriel <127857248+DanielBerriel@users.noreply.github.com>
2024-11-21 13:34:04 +0000

    adds `/api/v1/migrations` endpoint

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,10 +1,15 @@
 import migrationRunner from "node-pg-migrate"; //o migrationRunner será a função que roda o node-pg-migrate. Para isso precisamos passar um objeto como parâmetro dessa função, que tenha todas as informações necessárias para rodar nossas migrations. E como resultado retornar um objeto contendo as seguintes informações {path:,name:,timestamp:,}
 import { join } from "node:path"; //importamos a função join do node:path para garantir que a forma como que o caminho do arquivo está sendo passado seja compatível com o sistema operacional que está executando
+import database from "infra/database.js";
 
 export default async function migrations(request, response) {
   //criamos uma variável que carrega o objeto contendo as opções padrão das nossas migrations
+  const dbClient = await database.getNewClient();
   const defaultMigrationsOptions = {
-    databaseUrl: process.env.DATABASE_URL,
+    //ao instalar o módulo dotenv, demos ao node-pg-migration o poder de acessar o arquivo das variáveis de ambiente
+    //para conseguir interpolar variáveis dentro do .env.development usamos o dotenv-expand
+    //databaseUrl: process.env.DATABASE_URL,
+    dbClient: dbClient,
     dryRun: true,
     dir: join("infra", "migrations"),
     direction: "up",
@@ -18,6 +23,7 @@ export default async function migrations(request, response) {
 
   if (request.method === "GET") {
     const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end(); // Como estamos usando o dbClient para passar o client do banco de dados pra migrations, fica sob nossa responsabilidade fechar a connection; caso contrário após rodar as migrations o banco ficarar lock
     return response.status(200).json(pendingMigrations);
   }
 
@@ -27,6 +33,8 @@ export default async function migrations(request, response) {
       ...defaultMigrationsOptions,
       dryRun: false,
     });
+
+    await dbClient.end();
 
     if (migratedMigrations.length > 0) {
       //aqui estamos analisando se alguma migrations foi retornada (rodou/executou) ao chamarmos o endpoint /migrations. Se rodou retornamos o status 201(created)
